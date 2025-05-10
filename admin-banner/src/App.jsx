703dfe3318b0c2907cccecc1f61f3032f3f6f78b
mercysound <adekunleadewolemercy@gmail.com>
2025-05-10 18:02:10 +0100

    make dif

--- admin-banner/src/App.jsx
+++ admin-banner/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
 import Admin from './Pages/Admin/Admin'
+import Navbar from './Components/Navbar/Navbar'
 
 const App = () => {
   return (
